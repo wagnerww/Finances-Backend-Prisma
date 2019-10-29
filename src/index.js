d25a4e1790e322ae750bc7ce2753638f257b0a63
wagner <wagnerricardonet@gmail.com>
2019-10-29 13:33:46 -0300

    chore: implements jwt and bcrypt in auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const { GraphQLServer } = require("graphql-yoga");
+const Binding = require("prisma-binding");
 const { prisma } = require("./generated/prisma-client-js");
 
-const resolvers = {
-  Query: {
-    user(parent, args, context, info) {
-      return prisma.user({ id: args.id });
-    }
-  }
-};
+const resolvers = require("./resolvers");
 
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
-  resolvers
+  resolvers,
+  context: {
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint: process.env.PRISMA_ENDPOINT
+    }),
+    prisma
+  }
 });
 
 server
